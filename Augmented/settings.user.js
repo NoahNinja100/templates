edfbef1191d32a0bbe03d1a1b14154043b03e1fe
Noahninja100 <83727826+NoahNinja100@users.noreply.github.com>
2023-01-27 08:31:28 -0500

    Create settings.user.js

---
+++ Augmented/settings.user.js
@@ -0,0 +1,27 @@
+{
+    "window" : {
+        "title" : "No title set",
+        "name" : "userscript-gui",
+        "external" : false,
+        "centered" : false,
+        "size" : {
+            "width" : 300,
+            "height" : 500,
+            "dynamicSize" : true
+        }
+    },
+    "gui" : {
+        "centeredItems" : false,
+        "internal" : {
+            "darkCloseButton" : false,
+            "style" : `[...]`
+        },
+        "external" : {
+            "popup" : true,
+            "style" : `[...]`
+        }
+    },
+    "messages" : {
+        "blockedPopups" : () => alert(`[...]`)
+    }
+}
